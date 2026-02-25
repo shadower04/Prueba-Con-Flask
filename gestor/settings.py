303a9f8b1bee62df9f19a26dfd87fd52bf6e8746
Maximiliano Alzogaray <maxialzogaray700@gmail.com>
2026-02-25 17:06:28 -0300

    fix secret key render

--- gestor/settings.py
+++ gestor/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ee@2ik^(vxoh!d!!0egbvt6#krrnt&fkyxl(4015ivz=mb=(9t'
+SECRET_KEY =  os.environ.get('django-insecure-ee@2ik^(vxoh!d!!0egbvt6#krrnt&fkyxl(4015ivz', 'django-insecure-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
